bfc485461aeb7bd2c04d31cfbb9a6cd741baba1d
shalles <shalles@163.com>
2015-09-05 22:45:53 +0800

    add test & cmd.js

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # servermock
 node static server and mock data
+
+###Install
+
+```
+sudo npm install servermock -g  
+
+//启动命令(start)
+servermock start
+
+//or use as node module
+npm install servermock --save
+require('servermock')
+```
+
+###Config
+
+sm.config josn file in the project root or start path
+
+```json
+{
+    "port": 8080,
+    "protocol": 'http or https', //https\
+    "key": "~/cert/cert.key",
+    "cert": "~cert/cert.crt",
+    // mock请求
+    "mock":{
+        "datapath": "mock/",
+        "mockrc": ".mockrc", //相对mock datapath
+        "regexurl": { //前面是regex new RegExp()
+            "/api/mockdata1" : "mockdata.js", //走js 遵循cmd
+            "/api/mockdata1" : "mockdata.json", //
+            "/api/mockdata" : "mockdata.mjson" //
+        }
+    },
+}
+```
+**or**
+
+```js
+var servermock = require('servermock');
+servermock(config); // or use default config
+```
+
+###Mock
+
+support mockjson(.mjson) json(.json) function(req, res)(.js)
+
+**.mjson**
+
+```js
+
+```
+
+**.mockrc**
+
+```json
+{
+    "MCITY":["北京", "上海", "广州", "深圳", "贵阳", "厦门", "成都"],
+    "MNAME": ["shalles", "东阳", "小明", "小梅", "乔治"]
+}
+```
+
+detail look [  ]()
--- bin/cmd.js
+++ bin/cmd.js
@@ -1,3 +1,40 @@
 #!/usr/bin/env node
 
-var cmd = process.argv;
+var fs = require('fs'),
+    path = require('path'),
+    servermock = require('../index.js');
+
+var cmd = process.argv[2],
+    cwd = process.cwd();
+
+if(cmd == 'start'){
+    var config,
+        configPath = path.join(cwd, 'sm.config');
+
+    config = fs.existsSync(configPath) ? JSON.parse(fs.readFileSync(configPath)): {};
+    console.log('--------------------servermock start-------------------\n',
+                '===config===\n', 
+                config);
+
+    servermock(config);
+} else {
+    console.log('--------------------servermock-------------------\n',
+                '$ servermock start 启动\n', 
+                '===配置文件===build path下.smrc文件, 格式如下:\n', 
+                {
+                    port: 8080,
+                    protocol: 'http or https', //https\
+                    key: "~/cert/cert.key",
+                    cert: "~cert/cert.crt",
+                    // mock请求
+                    mock:{
+                        datapath: "mock/",
+                        mockrc: ".mockrc", //相对mock datapath
+                        regexurl: { //前面是regex new RegExp()
+                            "/api/placesuggestion" : "placesuggestion.js", //走js 遵循cmd
+                            "/api/placesuggestion" : "placesuggestion.json", //
+                            "/api/placesuggestion" : "placesuggestion.mjson" //
+                        }
+                    },
+                });
+}
