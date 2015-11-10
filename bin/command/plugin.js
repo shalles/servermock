4fb53a7dd02c9f4b15c6a2cff7257b3ad72f3be2
shalles <shalles@163.com>
2015-11-10 16:57:29 +0800

    opt plugin

--- bin/command/plugin.js
+++ bin/command/plugin.js
@@ -2,51 +2,70 @@ var fs = require('fs'),
     path = require('path'),
     utils = require('../../lib/utils.js');
 
+var plugin = {
+    intall: function(params){
+        var src = params[0];
+        if(!src){
+            utils.log(utils.chalk.red('缺少参数\n'),
+                "pligin install [git repository]"
+            );
+            return;
+        }
+    
+        var homeDir = utils.getUserDir();
+        var tempPath = path.join(homeDir, ".servermock/plugins_temp");
+    
+        utils.mkPath(tempPath);
+        
+        var name = src.slice(src.lastIndexOf('\/') + 1, -4),
+            tmpPluginPath = path.join(tempPath, name),
+            command = '';
+        //组织命令 删除旧临时文件 load plugin 
+        var time = '#';
+        var timer = setInterval(function(){
+            console.log(time+= '#');
+        }, 1200);
+        command += 'cd ' + tempPath + ' && ' + (fs.existsSync(tmpPluginPath) ? utils.cmd.rm + ' -rf * && ' : '') + 'git clone ' + src;
+        utils.excute(command, function (stdout, stderr) {
+            stdout && console.log(stdout), stderr && console.log(stderr);
+            try{
+                var pType = utils.readJson(path.join(tempPath, fs.readdirSync(tempPath)[0], 'package.json'))
+                                .servermock.type.replace(/[^a-zA-Z]/ig, '');
+            } catch (err){
+                utils.log('plugin not define type in package.json servermock.type ' + src);
+                return;
+            }
+            utils.log(utils.chalk.green('plugin ' + name + ' load success!'));
+            
+            var pluginTypePath = path.join(homeDir, '.servermock/plugins/', pType),
+                pluginPath = path.join(pluginTypePath, name);
+            utils.mkPath(pluginTypePath);
+    
+            command = (pluginPath !== '\/' && fs.existsSync(pluginPath) ? utils.cmd.rm + ' -rf ' + pluginPath + ' && ': '') + 
+                                utils.cmd.mv + " -f " + tmpPluginPath + ' ' + pluginTypePath;
+            utils.excute(command, function(stdout, stderr){
+                clearInterval(timer);
+                stdout && console.log(stdout), stderr && console.log(stderr);
+                utils.log(utils.chalk.green('plugin ' + name + ' installed successfully!'));
+            });
+        }, utils.chalk.red('load pligins error\t'));
+    }
+}
+
+plugin['-i'] = plugin.intall;
+plugin['-d'] = plugin.delete;
+
 module.exports = function(params){
-    var src = params[0];
-    if(!src){
-        utils.log(utils.chalk.red('缺少参数\n'),
-            "pligin [git repository]"
+    var cmd = params[0];
+    params = params.slice(1);
+    if(!plugin[cmd]){
+        utils.log('参数错误', 
+            '\nplugin [command]',
+            '\ninstall\t\tinstall servermock plugin 通过指定一个 git repository path',
+            '\ndelete\t\tdelete 用户的一个 servermock plugin from .servermock/plugins'
         );
         return;
     }
-
-    var homeDir = utils.getUserDir();
-    var tempPath = path.join(homeDir, ".servermock/plugins_temp");
-
-    utils.mkPath(tempPath);
     
-    var name = src.slice(src.lastIndexOf('\/') + 1, -4),
-        tmpPluginPath = path.join(tempPath, name),
-        command = '';
-    //组织命令 删除旧临时文件 load plugin 
-    var time = '#';
-    var timer = setInterval(function(){
-        console.log(time+= '#');
-    }, 1200);
-    command += 'cd ' + tempPath + ' && ' + (fs.existsSync(tmpPluginPath) ? utils.cmd.rm + ' -rf * && ' : '') + 'git clone ' + src;
-    utils.excute(command, function (stdout, stderr) {
-        stdout && console.log(stdout), stderr && console.log(stderr);
-        try{
-            var pType = utils.readJson(path.join(tempPath, fs.readdirSync(tempPath)[0], 'package.json'))
-                             .servermock.type.replace(/[^a-zA-Z]/ig, '');
-        } catch (err){
-            utils.log('plugin not define type in package.json servermock.type ' + src);
-            return;
-        }
-        utils.log(utils.chalk.green('plugin ' + name + ' load success!'));
-        
-        var pluginTypePath = path.join(homeDir, '.servermock/plugins/', pType),
-            pluginPath = path.join(pluginTypePath, name);
-        utils.mkPath(pluginTypePath);
-
-        command = (pluginPath !== '\/' && fs.existsSync(pluginPath) ? utils.cmd.rm + ' -rf ' + pluginPath + ' && ': '') + 
-                            utils.cmd.mv + " -f " + tmpPluginPath + ' ' + pluginTypePath;
-        utils.excute(command, function(stdout, stderr){
-            clearInterval(timer);
-            stdout && console.log(stdout), stderr && console.log(stderr);
-            utils.log(utils.chalk.green('plugin ' + name + ' installed successfully!'));
-        });
-    }, utils.chalk.red("load pligins error\t"));
-
+    plugin[cmd](params);
 }
