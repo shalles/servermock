35edbf794b3b1179f1f1465fd5d7c091f0fa3c35
shalles <shalles@163.com>
2015-11-18 18:40:48 +0800

    git diff

--- bin/command/start.js
+++ bin/command/start.js
@@ -7,6 +7,25 @@ var cwd = process.cwd();
 module.exports = function(params){
     var configPath = path.join(cwd, 'sm.config'),
         config = utils.readJson(configPath) || {};
+    
+    console.log("params: ", params)
+    var i = 0;
+    while(params[i]){
+        switch(params[i++]){
+            case '-p':
+                var port = parseInt(params[i++]);
+
+                if(!isNaN(port) && port > 0 && port < 65536){
+                    config.port = port;
+                }
+                break;
+            case '-i':
+                config.main = params[i++];
+                break;
+            default: 
+                break;
+        }
+    }
 
     utils.log(utils.chalk.green('start with config\n'), config);
 
